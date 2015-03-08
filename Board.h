fce3a88e695bf3a17bd3b2217819a530e6288c05
Leiniao1 <yinhangtsinghua@gmail.com>
2015-03-07 19:31:23 -0600

    Update1

---
+++ Board.h
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<Cell.h>
+#include<vector>
+using namespace std;
+
+class Board{
+private:
+	vector<vector<Cell>> cells_;
+	Board(int row_num, int col_num){
+		cells_.assign(row_num, vector<Cell>(col_num));
+	};
+};
---
+++ Cell.h
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+class Cell{
+public:
+	int input_color;
+	int curr_color;
+	bool bridge;
+
+};
