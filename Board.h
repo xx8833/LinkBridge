fd86a3c8a6ea2b8bf264ce9198ac8bc39afdcfbc
xx8833 <xuzy88@gmail.com>
2015-03-07 20:05:03 -0800

    Add load logic for board

--- Board.h
+++ Board.h
@@ -1,6 +1,9 @@
 #include<iostream>
+#include<string>
 #include"Cell.h"
 #include<vector>
+#include<fstream>
+#include<stdlib.h>
 using namespace std;
 
 class Board{
@@ -8,5 +11,26 @@ public:
 	vector<vector<Cell>> cells_;
 	Board(int row_num, int col_num){
 		cells_.assign(row_num, vector<Cell>(col_num));
+	}
+	static Board LoadProblem(string path)
+	{
+		std::ifstream ifs;
+	    ifs.open(path, std::ifstream::in);
+		if(!ifs.is_open())
+		{
+			cerr<<"Load File Open Failure.\n";
+			exit(-1);
+		}
+		int rows, columns;
+		ifs >> rows >> columns;
+		Board board(rows, columns);
+		for(auto & row : board.cells_)
+			for(auto & cell : row)
+		{
+			string cell_info;
+			ifs >> cell_info;
+			cell.Load(cell_info);
+		}
+		return board;
 	}
 };
--- Cell.h
+++ Cell.h
@@ -1,10 +1,14 @@
 #include<iostream>
 using namespace std;
 
+//S: 
 class Cell{
 public:
 	int color, colorb;
 	bool is_source;
 	bool bridge;
-
+	int Load(string input)
+	{
+		return 0;
+	}
 };
