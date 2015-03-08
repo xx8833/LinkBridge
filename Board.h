2f9c43b0133ee22e3971eaf0e7ad6967c59c8e35
Leiniao1 <yinhangtsinghua@gmail.com>
2015-03-07 19:38:29 -0600

    Change Board to public

--- Board.h
+++ Board.h
@@ -4,7 +4,7 @@
 using namespace std;
 
 class Board{
-private:
+public:
 	vector<vector<Cell>> cells_;
 	Board(int row_num, int col_num){
 		cells_.assign(row_num, vector<Cell>(col_num));
--- OutputGraph.cpp
+++ OutputGraph.cpp
@@ -1,7 +1,12 @@
+#include<iostream>
 #include<Board.h>
+using namespace std;
 
+void ToHtml(Board BD1){
 
+}
 
 int main(){
-	
+	Board BD(3,3);
+
 }
